d4b9d458fa56abea77a0bf74dc7248dd47a4960b
RandieSasongko <113977205+RandieSasongko@users.noreply.github.com>
2025-04-22 01:22:50 +0700

    Rollback

--- prepare_ntlk.py
+++
@@ -1,8 +0,0 @@
-import nltk
-
-nltk.download('stopwords', download_dir='/usr/local/nltk_data')
-nltk.download('punkt', download_dir='/usr/local/nltk_data')
-nltk.download('wordnet', download_dir='/usr/local/nltk_data')
-nltk.download('punkt_tab', download_dir='/usr/local/nltk_data')
-
-print("✅ NLTK data downloaded.")
